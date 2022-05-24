288c565b071a210ecd5150bd0ae81486ed72447d
naveed-shoukat <shoukat.naveed@gmail.com>
2022-05-25 02:58:46 +0300

    exercise1 add

--- react_by_free_code_camp/src/App.js
+++ react_by_free_code_camp/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Page from './Exercise1';
 
 function App() {
   return (
     <div className="App">
       <h1>This is my new React App</h1>
+      <Page />
     </div>
   );
 }
