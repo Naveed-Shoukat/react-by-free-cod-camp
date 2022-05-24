9f6837198f699f5d22f86484ec081e70e6914033
naveed-shoukat <shoukat.naveed@gmail.com>
2022-05-25 02:59:15 +0300

    exercise1 add

---
+++ react_by_free_code_camp/src/Exercise1.jsx
@@ -0,0 +1,15 @@
+function Page() {
+  return (
+    <div>
+      <h1>The new React App Heading from exercise1.jsx</h1>
+      <p>We are learning react from free-code-camp</p>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact Us</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Page;
