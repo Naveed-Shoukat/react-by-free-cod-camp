2669fe38f5dec80d7e7d4c98cd41f6f85f4abb1e
naveed-shoukat <shoukat.naveed@gmail.com>
2022-05-25 03:17:17 +0300

    exercise 2 done

--- react_by_free_code_camp/src/App.js
+++ react_by_free_code_camp/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
-import Page from './Exercise1';
+import Page from './components/Exercise1';
+import FunFact from './components/Exercise2';
 
 function App() {
   return (
     <div className="App">
       <h1>This is my new React App</h1>
-      <Page />
+      {/* <Page /> */}
+      <FunFact />
     </div>
   );
 }
---
+++ react_by_free_code_camp/src/components/Exercise2.jsx
@@ -0,0 +1,17 @@
+function FunFact() {
+  return (
+    <div>
+      {/* <img src="favicon.ico" alt="Logo" /> */}
+      <h1>Fun Fact about React</h1>
+      <ul>
+        <li>Rect was first release in 2013</li>
+        <li>React was originaly created by jorden Walke from facebook</li>
+        <li>Has well over 100k stars on GitHub</li>
+        <li>React was maintained by facebook</li>
+        <li>React Power's thousand of enterprises apps and mobile apps</li>
+      </ul>
+    </div>
+  );
+}
+
+export default FunFact;
