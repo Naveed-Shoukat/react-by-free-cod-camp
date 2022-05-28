94e3bf3d80b368832fed85e687b069c6435248bd
naveed-shoukat <shoukat.naveed@gmail.com>
2022-05-28 23:13:25 +0300

    exercise  final

--- react_by_free_code_camp/src/components/Exercise2.jsx
+++ react_by_free_code_camp/src/components/Exercise2.jsx
@@ -1,7 +1,7 @@
 function FunFact() {
   return (
     <div>
-      {/* <img src="favicon.ico" alt="Logo" /> */}
+      <img src="./logo192.png" alt="Logo" width="40px" />
       <h1>Fun Fact about React</h1>
       <ul>
         <li>Rect was first release in 2013</li>
@@ -10,6 +10,35 @@ function FunFact() {
         <li>React was maintained by facebook</li>
         <li>React Power's thousand of enterprises apps and mobile apps</li>
       </ul>
+      <ol>
+        <li>
+          <h4>Why we need to import React and ReactDOM</h4>
+          <p>
+            JSX Syntext defines in React. Technicaly we don't need to import
+            @version 18 and above. However the purpose behined this is the JSX
+            syntex was handled by React and thefore we need to import React and
+            ReactDOM. If we do not import React then our JSX will look like an
+            object in the actual DOM
+          </p>
+        </li>
+        <li>
+          <p>
+            If we console.log(JSX component/code) it will shown as a object in
+            with various properties and methods.
+          </p>
+        </li>
+        <li>
+          <p>
+            We can only return One main dive through JSX components. If we need
+            to return more than one components we simply need to wrap them in a
+            div or blank element i.e.&lt;&gt;&lt;/&gt;{' '}
+          </p>
+        </li>
+        <li>
+          <p>Declarative</p>
+          <p>imparative</p>
+        </li>
+      </ol>
     </div>
   );
 }
