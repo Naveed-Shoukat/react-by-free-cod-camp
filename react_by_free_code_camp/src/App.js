5d1b9f2231ce8ad671c8b2427dd788128c0c8fc3
naveed-shoukat <shoukat.naveed@gmail.com>
2022-05-29 00:31:13 +0300

    exercise 3 add

--- react_by_free_code_camp/src/App.js
+++ react_by_free_code_camp/src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
-import Page from './components/Exercise1';
-import FunFact from './components/Exercise2';
+// import Page from './components/Exercise1';
+// import FunFact from './components/Exercise2';
+import { MyElementOne, MyelementTwo } from './components/Exercise3';
 
 function App() {
   return (
     <div className="App">
       <h1>This is my new React App</h1>
+      {/* Exercise 1 is displayed as FunFact Element */}
       {/* <Page /> */}
       {/* Exercise 2 is displayed as FunFact Element */}
-      <FunFact />
+      {/* <FunFact /> */}
+      {/* Exercise 3 add two components instaed of only one*/}
+      <MyElementOne />
+      <MyelementTwo />
     </div>
   );
 }
